4bca2963468c827414e05a3e0d680cb35a655ec1
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2026-07-04 13:33:47 +0100

    refactor: intervals and rounds works as intended now

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,100 @@
 use clap::Parser;
-use rodio::Source;
 use rodio::source::SineWave;
+use rodio::{MixerDeviceSink, Source};
 use std::error::Error;
 use std::thread;
 use std::time::{Duration, Instant};
 
 // TODO
-// - [ ] cli flags: --interval 1m,30s
+// - [ ] cli flags and parse human-readable duration format:
+//          e.g. --intervals 1m 30s --lim 1h
+//          or --spar 5m --rest 2m --rounds 5
 // - [ ] print single formatted output string
-// - [ ] and...
+// - [ ] TUI showing grid of digital timers
 
 const ANSI_CLEAR: &str = "\x1B[2J\x1B[H";
 
+/// A simple time-keeping CLI app
 #[derive(Parser)]
-#[command(version, about)]
+#[command(
+    version,
+    after_help = "Examples:
+    cli-timer --intervals 10 20 30 --rounds 3
+    cli-timer --intervals 60 90
+    "
+)]
 struct Args {
+    /// List of intervals for every round
     #[arg(long, required=true, num_args=1..)]
-    rounds: Vec<String>,
+    intervals: Vec<String>,
+    /// Total number of rounds (default=1)
+    #[arg(long, required = false, num_args = 1)]
+    rounds: Option<usize>,
+}
+
+fn bell(device_sink: &MixerDeviceSink, tone: f32) {
+    let mixer = device_sink.mixer();
+    let wave = SineWave::new(tone)
+        .amplify(1.5)
+        .take_duration(Duration::from_secs_f32(1.5));
+    mixer.add(wave);
+    thread::sleep(Duration::from_secs_f32(1.5));
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let rounds: Vec<u64> = args
-        .rounds
+
+    let mut device_sink = rodio::DeviceSinkBuilder::open_default_sink()?;
+
+    let intervals: Vec<u64> = args
+        .intervals
         .iter()
         .filter_map(|s| Some(s.parse::<u64>().ok())?)
         .rev()
         .collect();
 
-    let stream_handle = rodio::DeviceSinkBuilder::open_default_sink()?;
-    let mixer = stream_handle.mixer();
+    // ensure rounds is greater than or equal to 1
+    let rounds = args.rounds.filter(|&x| x >= 1).unwrap_or(1);
 
     let start_time = Instant::now();
-    let mut last_time = Instant::now();
-    let mut current_time = Instant::now();
+    let mut last_time = start_time;
+
+    let mut current_time: Instant;
     let mut total_time: u64 = 0;
-    let mut round_time: u64 = rounds[0];
+    let mut round_time: u64;
 
-    let mut idx: usize = 0;
+    for round in 1..=rounds {
+        for (idx, current_interval) in intervals.iter().enumerate() {
+            let _idx = idx + 1;
+            println!("{ANSI_CLEAR} Next Interval: #{_idx} in {intervals:?}");
+            bell(&device_sink, 960.0);
 
-    loop {
-        // keep track of which round we're on
-        let interval = rounds[idx];
-        if round_time >= interval {
-            idx += 1;
-            if idx >= rounds.len() {
-                idx = 0;
+            loop {
+                current_time = Instant::now();
+                total_time = current_time.duration_since(start_time).as_secs();
+                round_time = current_time.duration_since(last_time).as_secs();
+
+                // keep track of which round we're on
+                if round_time >= *current_interval {
+                    last_time = current_time;
+                    break; // exit current interval
+
+                // output some words of encouragement
+                } else {
+                    println!("{ANSI_CLEAR}Keep Going!");
+                    println!("Round {round} out of {rounds}");
+                    println!("Interval #{_idx} in {intervals:?}");
+                    println!("Interval Time: {round_time}s out of {current_interval}s");
+                    thread::sleep(Duration::from_millis(100));
+                }
             }
-            // Generate bell
-            println!("{ANSI_CLEAR}Round start!");
-            last_time = current_time; // reset the interval tracker
-            let wave = SineWave::new(960.0)
-                .amplify(1.5)
-                .take_duration(Duration::from_secs_f32(1.5));
-            mixer.add(wave);
-            thread::sleep(Duration::from_secs_f32(1.5));
-        } else {
-            // output some words of encouragement
-            println!("{ANSI_CLEAR}Keep Going! (interval: {interval}s)");
-            println!("Total time: {total_time}s");
-            println!("Round time: {round_time}s");
-            thread::sleep(Duration::from_millis(100));
         }
-        current_time = Instant::now();
-        total_time = current_time.duration_since(start_time).as_secs();
-        round_time = current_time.duration_since(last_time).as_secs();
     }
+
+    println!("Well Done! (Total Time: {total_time}s)");
+    bell(&device_sink, 480.0);
+
+    device_sink.log_on_drop(false);
+
+    Ok(())
 }
